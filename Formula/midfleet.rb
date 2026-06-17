956636ee67393208a98b1d4abe14741aad8c899c
therajushahi <mail@rajushahi.com>
2026-06-17 19:17:41 +0800

    fix: link only midfleet binary

--- Formula/midfleet.rb
+++ Formula/midfleet.rb
@@ -11,7 +11,7 @@ class Midfleet < Formula
 
   def install
     system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink "#{libexec}/bin/midfleet"
   end
 
   def caveats
